91a4fb94396540485fe351b735d7066380a66f5c
JCV0827 <vjc2026@dlsud.edu.ph>
2025-02-19 09:33:33 +0800

    I HAVE BACK UP (READ THIS FIRST BEFORE PULLING) I HAVE ADDED TAILWIND FOR THIS PROJECT FOR MORE DESIGN CUSTOMIZATION THANKS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false, // Disable Tailwind's reset to avoid conflicts with Mantine
+  },
+}
