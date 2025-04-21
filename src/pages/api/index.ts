dd67814fd6b92fc51586216b0f5e647f20d720c5
JCV0827 <vjc2026@dlsud.edu.ph>
2025-04-21 19:06:48 +0800

    vercel node

---
+++ src/pages/api/index.ts
@@ -0,0 +1,9 @@
+const express = require("express");
+import { Request, Response } from 'express';
+const app = express();
+
+app.get("/", (req: Request, res: Response) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
